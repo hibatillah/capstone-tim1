b6f1790427f582997e72fe6985dd90f1e0940393
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-06-09 14:59:09 +0700

    setup talwind config

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Dashboard = () => {
   return (
     <>
-      
+      <p className="container btn btn-primary flex-fill">Dashboard</p>
     </>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx}"
@@ -6,13 +8,57 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      padding: {
+        DEFAULT: "1rem",
+        lg: "3rem",
+      },
+    },
+    fontFamily: {
+      outfit: ["Outfit", ...defaultTheme.fontFamily.sans],
     },
     extend: {
+      flex: {
+        fill: "1 0 auto",
+      },
       colors: {
         black: "#1F1F1F",
+        primary: "#775FFD",
+        secondary: "#6AD2FF",
+        tertiary: "#2B3674",
+        grey: {
+          light: "#F4F7FF",
+          DEFAULT: "#DCDCDC",
+          dark: "#A3AED0",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addComponents, theme }) {
+      addBase({
+        h1: { fontSize: theme("fontSize.3xl") },
+        h2: { fontSize: theme("fontSize.2xl") },
+        h3: { fontSize: theme("fontSize.xl") },
+        p: { color: theme("colors.grey.dark") },
+      });
+      addComponents({
+        ".btn": {
+          borderRadius: theme("borderRadius.lg"),
+          paddingInline: theme("spacing.6"),
+          paddingBlock: theme("spacing.2"),
+          fontWeight: theme("fontWeight.medium"),
+          "&.btn-primary": {
+            backgroundColor: theme("colors.primary"),
+            color: theme("colors.white"),
+          },
+          "&.btn-secondary": {
+            backgroundColor: theme("colors.inherit"),
+            color: theme("colors.primary"),
+            borderWidth: theme("spacing.px"),
+            borderColor: theme("colors.primary"),
+          },
+        },
+      });
+    }),
+  ],
 };
-
