d0db011fc7647b1980d1ab1d1dea7e41c852f6a6
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-06-10 14:25:58 +0700

    new route user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,23 +4,11 @@ import { Aside, Header, Navbar, Footer } from "./components";
 import { Home, Menu, CustomerOrder, About, Auth, Dashboard, Production, Supply, Order, Unknown } from "./pages";
 
 const App = () => {
-  const [user, setUser] = useState("admin");
+  const [user, setUser] = useState('admin')
 
   return (
     <Router>
-      {user === "customer" ? (
-        <div className="w-full min-h-screen bg-grey-light">
-          <Navbar />
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/order" element={<CustomerOrder />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<Unknown />} />
-          </Routes>
-          <Footer />
-        </div>
-      ) : (user === 'admin' || user === 'supplier') ? (
+      {user === "admin" || user === "supplier" ? (
         <div className="w-full min-h-screen flex gap-x-3 p-3 bg-grey-light">
           <nav className="flex-none flex flex-col justify-start items-center gap-20 px-5 py-10 w-[15%] max-h-[calc(100vh-1.5rem)] bg-white rounded-xl">
             <Aside />
@@ -39,9 +27,21 @@ const App = () => {
           </div>
         </div>
       ) : (
-        <main>
-          <Auth />
-        </main>
+        <div className="w-full min-h-screen p-3 bg-grey-light">
+          <Navbar />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route
+              path={user === "customer" ? `/order/:${user.id}` : "/order"}
+              element={<CustomerOrder />}
+            />
+            <Route path="/about" element={<About />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="*" element={<Unknown />} />
+          </Routes>
+          <Footer />
+        </div>
       )}
     </Router>
   );
