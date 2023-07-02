43d8ba4999ae78cbadd5bf9f8f1dbe2c5aa8947e
astridnda <astri21si@mahasiswa.pcr.ac.id>
2023-07-02 10:53:15 +0700

    Home

--- client/src/assets/img/index.js
+++ client/src/assets/img/index.js
@@ -1,7 +1,9 @@
 import merah404 from './merah404.png'
 import ungu404 from './ungu404.png'
+import RotteBakery from './RotteBakery.png'
 
 export {
   merah404,
   ungu404,
+  RotteBakery,
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {} from "react";
+import { RotteBakery } from "../assets/img";
 
 const Home = () => {
-  return (
-    <main className="main-customer">
-      hai
-    </main>
-  )
-}
 
-export default Home
+  return (
+    <main className="main-customer grid place-items-center">
+      <div className="card flex gap-12 w-[70%] min-h-[50%] xl:w-[50%]">
+        <img
+          src={RotteBakery}
+          alt="home-page"
+          className="flex-auto h-[400px] rounded-md object-cover object-top"
+        />
+        <div className="flex-[0_1_2/5] flex flex-col justify-center gap-y-8">
+          <div>
+            <h2 className="text-primary">Ada di Semua Suasana</h2>
+            <h1 className="text-primary">Rotte Bakery</h1>
+            <p className="w-4/5">
+            Rotte Bakery merupakan outlet toko yang menjual berbagai jenis roti, kue, 
+            dan cake dengan harga yang murah dengan 40 Outlet yang tersebar di seluruh Riau.
+            </p>
+          </div>
+            <button type="submit" className="btn btn-primary w-full uppercase">
+            Pesan
+            </button>
+            
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
