b81fa7194d3ea36e392e49efd443814dcc0fae90
astridnda <astri21si@mahasiswa.pcr.ac.id>
2023-07-04 22:11:46 +0700

    CustomerOrder

--- client/src/pages/CustomerOrder.jsx
+++ client/src/pages/CustomerOrder.jsx
@@ -5,14 +5,24 @@ const About = () => {
 
   return (
     <main className="main-customer grid place-items-center">
-      <div className="grid grid-cols-2 gap-32 w-[90%] min-h-[80%] ">
+      <div className="grid grid-cols-2 gap-32 w-[80%] min-h-[80%] ">
             <div classname="flex-[0_1_2/5] flex flex-col justify-center gap-y-8">
-              <h1 className="text-primary text-5xl py-1">Siapa Kami?</h1>
-              <br></br>
+              <h1 className="text-primary text-3xl py-1">Pesan Sekarang!</h1>
               <p className= "text-blue-900 text-xl">
-              Rotte bakery berdiri pada tahun 2016 lalu, yang didirikan oleh Bapak Syafrizal di Pekanbaru.
+              Pesan dan rasakan nikmatnya berbagai
+                <p className= "text-blue-900 text-xl"> 
+                  roti yang ada di Rotte Bakery 
+                </p>
               </p>
-            </div>            
+              <br></br>
+              <div classname="flex gap-4 ">
+                <button type="submit" className="btn btn-primary flex-initial text-xl ">
+                  Pesanan Berlangsung
+                </button>
+                <button className="btn btn-secondary text-xl">Riwayat Pesanan</button>
+              </div>     
+            </div> 
+                   
         </div>     
     </main>
   );
