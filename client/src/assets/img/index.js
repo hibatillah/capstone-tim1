f430d9a2d67c63bba11cac87e06df93c536db46d
astridnda <astri21si@mahasiswa.pcr.ac.id>
2023-07-06 01:07:32 +0700

    CustomerOrder

--- client/src/assets/img/index.js
+++ client/src/assets/img/index.js
@@ -3,6 +3,7 @@ import ungu404 from './ungu404.png'
 import RotteBakery from './RotteBakery.png'
 import snackBox1 from './snack-box-1.png'
 import RedBean from './red-bean.jpeg'
+import FitO from './Fit-o.jpeg'
 //import snackBox2 from './snack-box-2.png'
 
 export {
@@ -11,5 +12,6 @@ export {
   RotteBakery,
   snackBox1,
   RedBean,
+  FitO,
 //  snackBox2,
 }
--- client/src/pages/CustomerOrder.jsx
+++ client/src/pages/CustomerOrder.jsx
@@ -1,7 +1,9 @@
 import React, {} from "react";
 import {RedBean} from "../assets/img";
+import {FitO} from "../assets/img";
 
-const About = () => {
+
+const CustomerOrder = () => {
 
   return (
     <main className="main-customer grid justify-center py-20">
@@ -22,24 +24,51 @@ const About = () => {
                 </button>
               </div>     
             </div> 
-            <div className="card flex-none w-[800px]">
 
+            <div className="card flex-none w-[800px] h-[500px]">
+              <h2 className="text-yellow-500 py-4"> Pending </h2>
             <div className="grid grid-cols-2">
-              <div className="py-20">
+              <div className="py-7">
+              <div>
                <img
                 src={RedBean}
                 alt="customer-page"
-                className="flex-auto h-[150px] rounded-md object-cover object-top"
+                className="flex-auto h-[150px] "
                 />
+              </div>
+              <div className="py-10">
+                <img
+                src={FitO}
+                alt="customer-page"
+                className="flex-auto h-[150px]"
+                />
+              </div>
             </div>
-
-            
+            <div className="px-20 py-1"> 
+              <p>Kode Transaksi</p>
+              <h5 className="text-blue-950">###8472014721</h5>
+              <br></br>
+              <p>Tanggal Waktu</p>
+              <h5 className="text-blue-950"> 8 Juni 2023 - 13.32</h5>
+              <br></br>
+              <h5 className="text-blue-900"> 17.500</h5>
+              <h5 className="text-blue-900"> 6.500</h5>    
+              <br></br>
+              
+               <button type="submit" className="btn btn-primary flex-initial text-xl">
+                 Pesan
+               </button>
+               <button className="btn btn-secondary text-xl">
+                 Pesan
+               </button>
+                 
+            </div>
             </div>
           </div>
-        </div>     
+          </div>
     </main>
   );
 };
 
 //card flex-auto h-[900px] rounded-md object-cover object-top -translate-y-10 translate-x-10
-export default About;
+export default CustomerOrder;
